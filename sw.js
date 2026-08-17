9f399018288554b977d912dde2f7ba70d231d6aa
fixaai <fixaai.app@gmail.com>
2026-08-17 14:28:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
      É o que garante o que a Isabela pediu: questão nova aparece sozinha,
      sem ninguém baixar arquivo. Sem internet, usa a última cópia salva.
 */
-const VERSAO_APP   = "5cd8a433daf7";
+const VERSAO_APP   = "74a9ae1b8b06";
 const VERSAO_BANCO = "6d5776ea7153";
 const CACHE_APP   = "fixaai-app-" + VERSAO_APP;
 const CACHE_BANCO = "fixaai-banco";
